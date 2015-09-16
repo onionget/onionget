351788d7b95a218e18aaec0249ab5964c2c3c077
onionget <onionget@notreal.onion>
2015-09-16 09:06:52 -0400

    redundant check to make sure memory bytes are > 0, also check in allocate though

--- source/memorymanager.c
+++ source/memorymanager.c
@@ -30,7 +30,7 @@ void *secureAllocate(uint64_t bytesize)
  * When passed a void** that dereferences to a void* pointing to bytesize bytes, secureFree first clears the buffer pointed to by setting it to all nuills
  * and then it points the pointer to NULL. Returns 1 on success and 0 on error.  NOTE that memory must be a void** despite being a void* in the definition. TODO make sure pointer arithmetic is correct 
  * TODO make sure memory barrier is correctly implemented, and generally just look really close at this function, preferably replace it with one of the standards
- * for memory zero 
+ * for memory zero TODO make sure volatile use conforms with EXP32-C
  */
 int secureFree(void *memory, uint64_t bytesize)
 {
@@ -40,6 +40,11 @@ int secureFree(void *memory, uint64_t bytesize)
   
   if(memory == NULL){
     printf("Error: Something was NULL that shouldn't have been\n");
+    return 0;
+  }
+  
+  if(bytesize == 0){
+    printf("Error: Zero bytes of memory is invalid\n");
     return 0;
   }
 
