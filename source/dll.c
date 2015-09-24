43869ad4e4cce0b3de65d5984b315c780c6ca64d
onionget <onionget@notreal.onion>
2015-09-24 10:05:30 -0400

    general refactor and restructing of dll.c, should be well suited to connectionBank, might need to make changes for fileBank though I will think about that later first thing is to get connectionBank done

--- source/dll.c
+++ source/dll.c
@@ -10,33 +10,34 @@
 #include "ogEnums.h"
 
 
-
-typedef struct dllMember
+typedef struct dllSlot
 {
-  struct dllMember   *previous;
-  struct dllMember   *next;
-  void               *memberData; 
-}dllMember;
+  struct dllSlot   *previous;
+  struct dllSlot   *next;
+  void             *pointer; 
+}dllSlot;
 
 
 typedef struct dllPrivate{
   dllObject        publicDll; 
-  struct dllMember *head;
-  struct dllMember *tail;
-  uint32_t         count; 
+  struct dllSlot   *head;
+  struct dllSlot   *tail;
+  uint32_t         count; //deal with overflow potential ! TODO
 }dllPrivate;
 
+
 //public methods
-static int       insert(dllObject *this, int end, void *memberData);
+static int        spawnSlot(dllObject *this);
+static void       *withdrawPointer(dllObject *this);
+static int        depositPointer(dllObject *this, void *pointer);
 
+//private methods 
+static inline int insertInitial(dllObject *this, dllSlot *slot);
+static inline int insertHead(dllObject *this, dllSlot *slot);
 
-//private methods
-static int       insertInitial(dllObject *this, dllMember *member);
-static int       insertHead(dllObject *this, dllMember *member);
-static int       insertTail(dllObject *this, dllMember *member);
-static dllMember *newDllMember(void *memberPointer);
 
 /************ OBJECT CONSTRUCTOR ******************/
+
 
 /*
  * newDll returns NULL on error, or a new doubly linked list object (with head and tail NULL) on success 
@@ -45,159 +46,183 @@ dllObject* newDll(void)
 {
   dllPrivate *privateThis = NULL;
   
+  //allocate memory for the object + private properties 
   privateThis = (dllprivate *)secureAllocate(sizeof(*privateThis));
   if(privateThis == NULL){
     logEvent("Error", "Failed to allocate memory for linked list");
     return NULL; 
   }
   
+  //initialize public methods
+  privateThis->publicDll.spawnSlot       = &spawnSlot; 
+  privateThis->publicDll.withdrawPointer = &withdrawPointer;
+  privateThis->publicDll.depositPointer  = &depositPointer; 
+  
+  //initialize private properties 
   privateThis->head   = NULL; 
   privateThis->tail   = NULL;
   privateThis->count  = 0; 
   
-  privateThis->publicDll.insert = &insert; 
-  
+
+  //return public object
   return (dllObject *)privateThis;
 }
 
 
-/************ PUBLIC METHODS ******************/
 
+/************* PUBLIC METHODS ************/
+
+
+// LIST SCAFFOLDING  
 
 /*
- * insert returns 0 on error and 1 on success. Items may be inserted at end DLL_HEAD or DLL_TAIL, for the
- * start or end of the list, respectively. 
+ * spawnSlot adds an empty slot to the list, returns 0 on error and the current slot count on success
+ * TODO currently error handling on this can put it into a screwed state of being how I intend to use it,
+ * however I plan to redo error handling, so will leave it for now 
  */
-static int insert(dllObject *this, int end, void *memberData)
+static int spawnSlot(dllObject *this)
 {
   dllPrivate *private      = NULL 
-  dllMember  *member       = NULL;
+  dllSlot    *slot         = NULL;
   int        insertSuccess = 0;
   
   //first some sanity checking
-  if(this == NULL || memberData == NULL){
+  if(this == NULL){
     logEvent("Error", "Something was NULL that shouldn't have been");
     return 0; 
   }
   
   private = (dllPrivate *)this;
-
-  if(end != DLL_HEAD && end != DLL_TAIL){
-    logEvent("Error", "end must be DLL_HEAD or DLL_TAIL");
-    return 0; 
-  }
   
-  //sanity checks passed so allocate a new member
-  member = newDllMember(memberData);
-  if(member == NULL){
-    logEvent("Error", "Failed to create dll member");
+  //sanity checks passed so allocate a new slot
+  slot = (dllSlot *)secureAllocate(sizeof(*slot));
+  if(slot == NULL){
+    logEvent("Error", "Failed to create dll slot");
     return 0;
   }
-
-
-  //insert the item into the list according to the end specified 
-  insertSuccess = 0; 
   
-  if      ( private->head == NULL     )  insertSuccess = insertInitial ( this , member ); 
-  else if ( end           == DLL_HEAD )  insertSuccess = insertHead    ( this , member );
-  else if ( end           == DLL_TAIL )  insertSuccess = insertTail    ( this , member ); 
+  slot->pointer = NULL; 
+
+  //insert the open slot to the head of the list
+  insertSuccess = (private->head == NULL) ? insertInitial(this, slot) : insertHead(this, slot); 
   
   if(!insertSuccess){
-    secureFree(&member, sizeof(*member)); 
-    logEvent("Error", "Failed to insert member into list");
+    secureFree(&slot, sizeof(*slot)); 
+    logEvent("Error", "Failed to insert slot into list");
     return 0; 
   }
   
-  //keep track of the total bytesize of the linked list
-  private->count += 1;
+  private->count += 1; 
   
-  return 1; 
+  
+  return private->count; 
 }
 
 
-/************ PRIVATE METHODS ******************/
 
-/*
- * newDllObject returns NULL on error, or a pointer to a new dllObject on success. 
+// LIST DATA
+
+/* 
+ * Checks slots from head and returns the pointer held by the first slot that holds a pointer to something besides NULL, or a NULL pointer on error or all slots empty 
+ * NOTE: for thread safety access to this must be blocked with a mutex (do so at a higher level of abstraction) 
  */
-static dllMember *newDllMember(void *memberData)
-{ 
-  dllMember *member;
+static void *withdrawPointer(dllObject *this)
+{
+  dllPrivate *private = NULL;
+  dllSlot    *slot    = NULL;
+  void       *holder  = NULL; 
   
-  //allocate the dllObject 
-  member = (dllMember *)secureAllocate(sizeof(*member));
-  if(member == NULL){
-    logEvent("Error", "Failed to allocate memory for linked list member");
-    return NULL;
+  if(this == NULL){
+    logEvent("Error", "Something was NULL that shouldn't have been");
+    return NULL; 
   }
   
-  member->memberData    = memberData; 
+  private = (privateDll *)this;
   
-  return member;
+  for(slot = private->head ; slot ; slot = slot->next){
+    if(slot->pointer != NULL){
+      holder     = slot->pointer;
+      slot->pointer = NULL;
+      return holder; 
+    }
+  }
+  
+  return NULL; 
 }
 
+
+//checks slots from head and adds item to first open slot available, returns -1 on error, 1 on success, and 0 if no open slots
+static int depositPointer(dllObject *this, void *pointer)
+{
+  dllPrivate *private = NULL;
+  dllSlot    *slot    = NULL;
+  
+  if(this == NULL || pointer == NULL){
+    logEvent("Error", "Something was NULL that shouldn't have been");
+    return -1; 
+  }
+  
+  private = (dllPrivate *)this;
+  
+  for(slot = private->head ; slot ; slot = slot->next){
+    if(slot->pointer == NULL){
+      slot->pointer = pointer;
+      return 1; 
+    }
+  }
+  
+  return 0; 
+}
+
+
+
+
+/**** PRIVATE METHODS *****/ 
+
+
+// LIST SCAFFOLDING  
+
 /*
- * insertInitial returns 0 on error and 1 on success. It's for inserting the initial item into a linked list. 
+ * insertInitial adds the first slot to a list that has no slots in it. Returns 0 on error and 1 on success. 
  */
-static inline int insertInitial(dllObject *this, dllMember *member)
+static inline int insertInitial(dllObject *this, dllSlot *slot)
 {
   privateDll *private = NULL;
   
-  if(this == NULL || member == NULL){
+  if(this == NULL || slot == NULL){
     logEvent("Error", "Something was NULL that shouldn't have been");
     return 0;
   }
   
   private = (dllPrivate *)this;
   
-  private->head       = member;
-  private->tail       = member;
-  member->next        = NULL;
-  member->previous    = NULL;    
+  private->head     = slot;
+  private->tail     = slot;
+  slot->next        = NULL;
+  slot->previous    = NULL;    
   
   return 1;
 }
 
+
 /*
- * insertHead returns 0 on error and 1 on success. It inserts an item at the head of the list. 
+ * insertHead adds a slot to the head of a list that has at least one slot already in it. Returns 0 on error and 1 on success. 
  */
-static inline int insertHead(dllObject *this, dllMember *member)
+static inline int insertHead(dllObject *this, dllSlot *slot)
 {
   privateDll *private = NULL;
   
-  if(this == NULL || member == NULL){
+  if(this == NULL || slot == NULL){
     logEvent("Error", "Something was NULL that shouldn't have been");
     return 0;
   }
   
   private = (dllPrivate *)this;
   
-  private->head->previous = member;
-  member->next            = private->head;
-  member->previous        = NULL;
-  private->head           = member;
+  private->head->previous = slot;
+  slot->next              = private->head;
+  slot->previous          = NULL;
+  private->head           = slot;
   
-  return 1;
-}
-
-/*
- * insertTail returns 0 on error and 1 on success. It's for inserting an item as the tail of the list. 
- */
-static inline int insertTail(dllObject *this, dllMember *member)
-{
-  privateDll *private = NULL;
-  
-  if(this == NULL || member == NULL){
-    logEvent("Error", "Something was NULL that shouldn't have been");
-    return 0;
-  }
-  
-  private = (dllPrivate *)this;
-
-  private->tail->next = member;
-  member->previous    = private->tail;
-  member->next        = NULL;
-  private->tail       = member;
- 
   return 1;
 }
--- source/dll.h
+++ source/dll.h
@@ -1,12 +1,11 @@
 #pragma once
 #include <stdint.h>
-#include "dataContainer.h"
-
-
 
 
 typedef struct dllObject{  
-  int (*insert)(struct dllObject *this, int end, void *memberData);  
+  int (*spawnSlot)(struct dllObject *this);
+  int (*depositPointer)(struct dllObject *this, void *pointer);
+  void *(*withdrawPointer)(struct dllObject this);
 }dllObject;
 
 
